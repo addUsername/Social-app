3a772d6613ce1577a06f91cb596dd5e4f0d26ac8
addUsername <60299373+addUsername@users.noreply.github.com>
2020-09-02 14:59:41 +0200

    Fix roles

--- back/src/main/java/com/addusername/social/dto/NewClient.java
+++ back/src/main/java/com/addusername/social/dto/NewClient.java
@@ -1,10 +1,13 @@
 package com.addusername.social.dto;
 
-import java.util.Set;
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor@Getter@Setter
 public class NewClient {
 
     @NotBlank
@@ -20,78 +23,6 @@ public class NewClient {
     @NotBlank
     private String password;
 
-    
-    private Set<String> roles;
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
-
-	/**
-	 * @param email the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	/**
-	 * @return the user name
-	 */
-	public String getUsername() {
-		return username;
-	}
-
-	/**
-	 * @param username the username to set
-	 */
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
-	}
-
-	/**
-	 * @param password the password to set
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	/**
-	 * @return the roles
-	 */
-	public Set<String> getRoles() {
-		return roles;
-	}
-
-	/**
-	 * @param roles the roles to set
-	 */
-	public void setRoles(Set<String> roles) {
-		this.roles = roles;
-	}
-    
     
 }
