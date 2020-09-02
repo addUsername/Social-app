19d27fcc8d1214900b633b95d61b401a1ba41a79
addUsername <60299373+addUsername@users.noreply.github.com>
2020-09-02 14:56:14 +0200

    Add home

--- vue/src/src/App.vue
+++ vue/src/src/App.vue
@@ -3,11 +3,27 @@
     <!--
       <Login />
        -->
-       <transition>
-        <keep-alive>
-          <router-view></router-view>
-        </keep-alive>
-      </transition>
+    <v-app-bar color="primary" dense dark>
+      <v-app-bar-nav></v-app-bar-nav>
+      <v-toolbar-title>Page title</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <router-link to="/login">
+        <v-btn>
+          <span>IN</span>
+        </v-btn>
+      </router-link>
+      <router-link to="/register">
+        <v-btn>
+          <span>UP</span>
+        </v-btn></router-link
+      >
+    </v-app-bar>
+    <v-main>
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </v-main>
   </v-app>
 </template>
 
